6f18b85cbe6ed03440ccc6ebab7c30469c4fa06b
Luca Radio <luca.1407@hotmail.it>
2022-12-02 16:40:37 +0100

    Initialize Reporistory

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import TheNavVue from './components/TheNav.vue';
+import Banner from './components/Banner.vue';
+import TheFooter from './components/TheFooter.vue';
+import SocialBanner from './components/SocialBanner.vue';
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    TheNavVue,
+    TheFooter,
+    Banner,
+    SocialBanner
+  }
+}
+
+
+</script>
+
+<template>
+  <TheNavVue></TheNavVue>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss"
+</style>
